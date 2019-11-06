f0440ba7ece8962af5120454e9bf76988482ffea
dandantas <dantasmaarotti@gmail.com>
2019-11-06 18:07:17 -0300

    Add reset password functionality + tests

--- backend/test/functional/forgotPassword.spec.js
+++ backend/test/functional/forgotPassword.spec.js
@@ -1,46 +1,76 @@
-const { test, trait } = use('Test/Suite')('Forgot Password')
+const { test, trait, beforeEach, afterEach } = use('Test/Suite')('Forgot Password')
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const User = use('App/Models/User');
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use('Factory');
+const Hash = use('Hash')
 
 const Mail = use('Mail')
 
+
 trait('Test/ApiClient')
-//  wraps all of your databases queries inside a transaction and roll it back after each test.
 trait('DatabaseTransactions')
 
-
-test('it should send an email', async ({ assert, client }) => {
-
+beforeEach(() => {
     Mail.fake()
+})
+afterEach(() => {
+    Mail.restore()
+})
 
-    const forgotPayload = {
-        email: 'dantasmaarotti@gmail.com',
-    }
-
+async function generateForgotPasswordToken(client, email) {
     const user = await Factory.
         model('App/Models/User').
-        create(forgotPayload);
+        create({ email });
 
-    const response = await client.
+    await client.
         post('/forgot').
-        send(forgotPayload).
-        end()
-
-
-    response.assertStatus(204)
-
-    const recentEmail = Mail.pullRecent()
-
-    assert.equal(recentEmail.message.to[0].address, forgotPayload.email)
+        send({ email }).
+        end();
 
     const token = await user.tokens().first();
 
+    return token;
+}
+
+test('it should send an email', async ({ assert, client }) => {
+    const email = 'dantasmaarotti@gmail.com';
+
+    const token = await generateForgotPasswordToken(client, email)
+
+    const recentEmail = Mail.pullRecent()
+
+    assert.equal(recentEmail.message.to[0].address, email)
     assert.include(token.toJSON(), {
-        user_id: user.id,
         type: 'forgotpassword'
     })
 
-    Mail.restore()
 });
+
+// entra numa rota nova de reset /reset
+// usuário digita a nova senha 
+// confirma a senha
+// muda a senha definitivamente
+// caso seja feita a requisição passado 2h da criação do token, dar erro
+
+test('it should reset password definitively', async ({ assert, client }) => {
+    const email = 'dantasmaarotti@gmail.com';
+
+    const { token } = await generateForgotPasswordToken(client, email)
+
+    const response = await client.post('/reset')
+        .send({
+            token,
+            password: '123456',
+            password_confirmation: '123456'
+        })
+        .end()
+
+    response.assertStatus(204)
+
+    const user = await User.findBy('email', email);
+    const checkPassword = await Hash.verify('123456', user.password);
+
+    assert.isTrue(checkPassword);
+
+})
